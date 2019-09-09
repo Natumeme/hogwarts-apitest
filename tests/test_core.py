ace57f072229efb07cb0c36bac2b67b60506a1f0
Natumeme <1466510866@qq.com>
2019-09-09 23:23:18 +0800

    update

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,18 +6,21 @@ import requests
 class BaseApi(object):
     pass
 
-class ApihttbinGet:
+class ApihttbinGet(BaseApi):
     url = "http://httpbin.org/get"
     params=None
     method = "GET"
     headers = {"accept":"application/json"}
 
-    def set_params(self):
-        self.params = {}
+    def set_params(self,**params):
+        self.params = params
         return self
 
     def run(self):
-        self.response = requests.get(self.url,self.headers)
+        self.response = requests.get(
+            self.url,
+            headers=self.headers,
+            params=self.params)
         return self
 
     def validate(self,key,expected_value):
@@ -25,7 +28,7 @@ class ApihttbinGet:
         assert actual_value == expected_value
         return self
 
-class ApihttpbinPost:
+class ApihttpbinPost(BaseApi):
     url = "http://httpbin.org/post"
     method = "POST"
     headers = {"accept": "application/json"}
@@ -47,6 +50,7 @@ def test_httpbin_get():
     # .validate("json.url","http://httpbin.org/post")
 
 def test_httpbin_get_with_params():
-    ApihttbinGet().set_params().run().\
-        validate()
+    ApihttbinGet().set_params(abc=123,xyz=456)\
+        .run()\
+        .validate("status_code",200)
 
