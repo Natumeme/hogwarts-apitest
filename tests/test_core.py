0d303f8ba81323a0e130852fd5281568ba4ae3e0
Natumeme <1466510866@qq.com>
2019-09-11 00:29:47 +0800

    update

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,34 +3,8 @@
 
 import requests
 
-class BaseApi(object):
-    method = "GET"
-    url = ""
-    params = {}
-    headers = {}
-    data = ""
-    json = {}
+from apitest.api import BaseApi
 
-    def set_params(self,**params):
-        self.params = params
-        return self
-
-    def run(self):
-        self.response = requests.request(
-            self.method,
-            self.url,
-            params=self.params,
-            headers=self.headers,
-            data=self.data,
-            json=self.json
-
-        )
-        return self
-
-    def validate(self,key,expected_value):
-        actual_value = getattr(self.response,key)
-        assert actual_value == expected_value
-        return self
 
 class ApihttbinGet(BaseApi):
     url = "http://httpbin.org/get"
@@ -57,15 +31,6 @@ class ApihttpbinPost(BaseApi):
     headers = {"accept": "application/json"}
     data = "abc=123"
     json = {"abc":123}
-
-    def set_data(self,data):
-        self.data = data
-        return self
-
-
-    def set_json(self,json_data):
-        self.json = json_data
-        return self
 
 def test_httpbin_post():
     ApihttpbinPost().set_json({"abc":456}).run().validate("status_code", 200)
