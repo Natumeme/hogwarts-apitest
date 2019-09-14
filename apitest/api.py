74f4819dd4e43c77d64019dc14bc081fbfdc776e
Natumeme <1466510866@qq.com>
2019-09-14 11:07:27 +0800

    Base类

--- apitest/api.py
+++ apitest/api.py
@@ -38,15 +38,15 @@ class BaseApi(object):
     def validate(self,key,expected_value):
         value = self.response
         for _key in key.split("."):
-            print("====",_key,value)
+            #print("====",_key,value)
             if isinstance(value,requests.Response):
                 if _key == "json()":
                     value = self.response.json()
                 else:
                     value = getattr(value,_key)
-            elif isinstance(value,requests.structures.CaseInsensitiveDict):
+            elif isinstance(value,(requests.structures.CaseInsensitiveDict,dict)):
                 value = value[_key]
 
-        #print("value----",value)
+        #print("=====222",_key,value,expected_value)
         assert value == expected_value
         return self
